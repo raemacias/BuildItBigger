fac30a3b0ddb0d32741ac878f57ba5dc7ee4da08
Rae <raemmacias@gmail.com>
2018-10-07 17:54:49 -0600

    Fixed error in EndpointAsyncTask

--- androidlibrary/src/main/java/com/raemacias/androidlibrary/JokeActivity.java
+++ androidlibrary/src/main/java/com/raemacias/androidlibrary/JokeActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
 public class JokeActivity extends AppCompatActivity{
 
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,19 +2,30 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.v4.app.Fragment;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.raemacias.androidlibrary.JokeActivity;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 import static android.content.Intent.EXTRA_TEXT;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PASSED_JOKE = "Joke";
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +58,81 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view) {
-
-        new EndpointAsyncTask().execute();
+//    public void tellJoke(View view) {
+//
+//        new EndpointsAsyncTask().execute();
 //        Context context = this;
 //        CharSequence text = this.getString(R.string.toast_text);
 //        int duration = Toast.LENGTH_LONG;
 //
 //        Toast toast = Toast.makeText(context, text, duration);
 //        toast.show();
+//    }
+
+//    @Override
+//    protected void onPostExecute(String result) {
+//        Intent jokeIntent = new Intent(context, JokeActivity.class);
+//        jokeIntent.putExtra(JokeActivity.JOKE_KEY, result);
+//        context.startActivity(jokeIntent);
+//    }
+
+
+    public void tellJoke(View view) {
+
+        // Call the GCE api for a joke
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "placeholder"));
     }
 
-    public void onFinished(String result) {
-        Intent jokeIntent = new Intent(this, JokeActivity.class);
-        jokeIntent.putExtra(EXTRA_TEXT, result);
-        startActivity(jokeIntent);
+
+}
+
+
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 
+    @Override
+    protected void onPostExecute(String result) {
+        final String PASSED_JOKE = "Joke";
 
+        // Create an intent to pass info
+        Intent myIntent = new Intent(context, JokeActivity.class);
+
+        // Pass the joke to the activity's intent
+        myIntent.putExtra(PASSED_JOKE, result);
+
+        // start activity display
+        context.startActivity(myIntent);
+    }
 }
